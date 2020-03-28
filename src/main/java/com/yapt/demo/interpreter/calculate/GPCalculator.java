4e5b27502ba68ebfc61313ab7d12194aea87f6a8
user.name <15377130973@163.com>
2020-03-28 19:51:15 +0800

    fix

--- src/main/java/com/yapt/demo/interpreter/calculate/GPCalculator.java
+++ src/main/java/com/yapt/demo/interpreter/calculate/GPCalculator.java
@@ -1,5 +1,9 @@
 package com.yapt.demo.interpreter.calculate;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class GPCalculator {
@@ -10,25 +14,72 @@ public class GPCalculator {
     }
 
     private void parse(String expression) {
-        String [] elements = expression.split(" ");
+        List<String> elements = process(expression);
         IArithmeticInterpreter leftExpr, rightExpr;
 
-        for (int i = 0; i < elements.length ; i++) {
-            String operator = elements[i];
+        for (int i = 0; i < elements.size() ; i++) {
+            String operator = elements.get(i);
             if (OperatorUtil.isOperator(operator)){
                 leftExpr = this.stack.pop();
-                rightExpr = new NumInterpreter(Integer.valueOf(elements[++i]));
+                rightExpr = this.stack.pop();
                 System.out.println("出栈: " + leftExpr.interpret() + " 和 " + rightExpr.interpret());
                 this.stack.push(OperatorUtil.getInterpreter(leftExpr, rightExpr,operator));
                 System.out.println("应用运算符: " + operator);
-            }
-            else{
-                NumInterpreter numInterpreter = new NumInterpreter(Integer.valueOf(elements[i]));
+            } else{
+                NumInterpreter numInterpreter = new NumInterpreter(Integer.valueOf(elements.get(i)));
                 this.stack.push(numInterpreter);
                 System.out.println("入栈: " + numInterpreter.interpret());
             }
         }
     }
+
+
+    /**
+     * 将中缀表达式转成后缀表达式对应的list
+     * @param str
+     * @return
+     */
+    public static List<String> process(String str) {
+        String[] array = str.split(" ");
+        // 符号栈
+        Stack<String> stack = new Stack<String>();
+        //存储中间结果的list
+        List<String> list = new ArrayList<String>();
+
+        //遍历 array
+        for(String item : array) {
+            System.out.println("输入的字符:"+item);
+            //如果是一个数字，就加入到list
+            if(item.matches("\\d+")) {
+                list.add(item);
+            } else if (item.equals("(")) {
+                stack.push(item);
+            } else if (item.equals(")")) {
+                // 如果是右括号，则依次弹出stack栈顶的运算符，并压入 list,直到遇到左括号为止，此时将这一对括号丢弃
+                while(!stack.peek().equals("(")) {
+                    list.add(stack.pop());
+                }
+                // 将左括号 弹出，消除小括号
+                stack.pop();
+            } else {
+                // 当 item 的优先级小于或等于栈顶运算符，将stack栈顶的运算符弹出并压入list中
+                while(stack.size() != 0 && OperatorUtil.getValue(stack.peek()) >= OperatorUtil.getValue(item)) {
+                    list.add(stack.pop());
+                }
+                //还需要将 item 压入 栈中
+                stack.push(item);
+            }
+            System.out.println("符号栈结果:"+stack);
+            System.out.println("后缀式结果:"+list);
+            System.out.println("***********************************************");
+        }
+        //将stack中剩余的运算符依次弹出加入list
+        while(stack.size()!=0) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+
 
     public int calculate() {
         return this.stack.pop().interpret();
--- src/main/java/com/yapt/demo/interpreter/calculate/OperatorUtil.java
+++ src/main/java/com/yapt/demo/interpreter/calculate/OperatorUtil.java
@@ -2,8 +2,36 @@ package com.yapt.demo.interpreter.calculate;
 
 public class OperatorUtil {
 
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    // 写一个方法，返回对应的优先级数字
+    public static int getValue(String operation) {
+        int result = 0;
+        switch(operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                System.out.println("不存在该运算符");
+                break;
+        }
+        return result;
+    }
+
     public static boolean isOperator(String symbol) {
-        return (symbol.equals("+") || symbol.equals("-") || symbol.equals("*"));
+        return (symbol.equals("+") || symbol.equals("-") || symbol.equals("*") || symbol.equals("/"));
     }
 
     public static Interpreter getInterpreter(IArithmeticInterpreter left, IArithmeticInterpreter right, String symbol) {
